e07355d7f70a165f50a1fa66a88f96e4d42d9414
MdMehedi-Hasan <mh19115@gmail.com>
2022-12-27 16:38:04 +0600

    testing pdf

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,136 @@
 import React from 'react';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 const Dashboard = () => {
+    const doc = new jsPDF();
+    /* const doc = new jsPDF();
+    const input = document.getElementById('divToPrint');
+    html2canvas(input)
+      .then((canvas) => {
+        let imgWidth = 208;
+        let imgHeight = canvas.height * imgWidth / canvas.width;
+        const imgData = canvas.toDataURL('img/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+        // pdf.output('dataurlnewwindow');
+        pdf.save("download.pdf");
+      }) */
+    const downloadPDF = () => {
+        const input = document.getElementById('divToPrint');
+        html2canvas(input)
+            .then((canvas) => {
+                let imgWidth = 208;
+                let imgHeight = canvas.height * imgWidth / canvas.width;
+                const imgData = canvas.toDataURL('img/png');
+                const pdf = new jsPDF('p', 'mm', 'a4');
+                pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+                pdf.save("download.pdf");
+            })
+    }
     return (
-        <div>
-            <p>This is a dashboard</p>
+        <div >
+            <table id="divToPrint" style={{ display: 'flex', justifyContent: 'center' }}>
+                <tbody style={{ border: '1px solid black' }}>
+                    <tr><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+                </tbody>
+            </table>
+            <button type="" onClick={() => downloadPDF()}>Download</button>
         </div>
     );
 };
